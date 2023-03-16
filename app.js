bdc855e5450e39b330ee75b882b708f0a228a4a4
Mohammed Shujauddin <shujath786@hotmail.com>
2023-03-16 00:46:33 -0400

    changed message

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ Iconst express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello Mohammed!How R U?'));
+app.get('/', (req, res) => res.send('Hey Mohammed!How R U?'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
